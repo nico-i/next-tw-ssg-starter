4f309d6f9de7fc85259016efd738cb9ceccd694e
Nico Ismaili <nico@ismaili.de>
2024-01-29 23:36:46 +0100

    feat: added bundle analyzer, dotenv and fixed sitemap gen /spent 30min

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,43 @@
+import BundleAnalyzer from "@next/bundle-analyzer"; // Change require to import
+import "dotenv/config";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withBundleAnalyzer = BundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
 
-export default withNextIntl(nextConfig);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // SVGR
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg"),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+};
+
+export default withNextIntl(withBundleAnalyzer(nextConfig));
